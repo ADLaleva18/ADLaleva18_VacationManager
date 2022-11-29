62ded6d030bf6413cbe1a4a07b0e9ac874ad3ce8
Aleksandra Laleva <58171958+ADLaleva18@users.noreply.github.com>
2022-11-29 10:32:13 +0200

    add database and diagram

---
+++ Database/databaseVM.sql
@@ -0,0 +1,94 @@
+USE master
+GO
+
+CREATE DATABASE [Vacation Manager]
+GO
+
+USE [Vacation Manager]
+
+CREATE TABLE [Roles]
+(
+	RoleId  INT PRIMARY KEY NOT NULL,
+	RoleName VARCHAR(250) NOT NULL
+);
+
+CREATE TABLE Users
+(
+	UserId INT PRIMARY KEY NOT NULL,
+	UserName VARCHAR(255) NOT NULL,
+	Email VARCHAR(255) NOT NULL,
+	PhoneNumber VARCHAR(255) NOT NULL,
+	[Password] VARCHAR(255) NOT NULL,
+	FirstName VARCHAR(50) NOT NULL,
+	LastName VARCHAR(50) NOT NULL,
+	DateJoined VARCHAR(50) NOT NULL,
+	RoleId INT NULL,
+	FOREIGN KEY (RoleId) REFERENCES Roles(RoleId)
+);
+
+CREATE TABLE Teams
+(
+	TeamId INT NOT NULL PRIMARY KEY,
+	TeamName VARCHAR(255) NULL,
+	UserId INT NOT NULL,
+	FOREIGN KEY (UserId) REFERENCES Users(UserId)
+);
+
+CREATE TABLE Projects
+(
+	ProjectId INT PRIMARY KEY NOT NULL,
+	ProjectName VARCHAR(255) NULL,
+);
+
+CREATE TABLE UsersTeams
+(
+   UserTeamId INT NOT NULL PRIMARY KEY,
+   UserId INT NOT NULL,
+   TeamId INT NOT NULL,
+   FOREIGN KEY (UserId) REFERENCES Users(UserId),
+   FOREIGN KEY (TeamId) REFERENCES Teams(TeamId)
+);
+
+CREATE TABLE TeamProjects
+(
+   TeamProjectId INT NOT NULL PRIMARY KEY,
+   TeamId INT NOT NULL,
+   ProjectId INT NOT NULL,
+   FOREIGN KEY (TeamId) REFERENCES Teams(TeamId),
+   FOREIGN KEY (ProjectId) REFERENCES Projects(ProjectId)
+);
+
+CREATE TABLE Vacations
+(
+	VacationId INT NOT NULL PRIMARY KEY,
+	RequestingEmployeeId INT NOT NULL,
+	StartDate DATE NULL,
+	EndDate DATE NULL,
+	DateRequested DATETIME DEFAULT CURRENT_TIMESTAMP,
+	DateActioned DATETIME,
+	Approved VARCHAR(10) NOT NULL,
+	ApprovedById INT NOT NULL,
+	Cancelled VARCHAR(10) NOT NULL,
+	UserId INT NOT NULL,
+	FOREIGN KEY (UserId) REFERENCES Users(UserId)
+);
+
+CREATE TABLE VacationTypes
+(
+UserId INT NOT NULL PRIMARY KEY,
+	Name VARCHAR(50) NOT NULL,
+	DateCreated DATETIME,
+	DefaultDays INT NOT NULL,
+	FOREIGN KEY (UserId) REFERENCES Users(UserId)
+);
+
+CREAtE TABLE VacationAllocations
+(
+	UserId INT NOT NULL PRIMARY KEY,
+	NumberOfDays INT NOT NULL,
+	DateCreated DATETIME,
+	EmployeeId INT NOT NULL,
+	LeaveTypeId INT NOT NULL,
+	FOREIGN KEY (LeaveTypeId) REFERENCES VacationTypes(UserId),
+	FOREIGN KEY (UserId) REFERENCES Users(UserId)
+);
